7b16904bbab231f6549af3d9971ee618284059ce
davidesidoti <sidotidavide@gmail.com>
2024-11-24 22:18:44 +0100

    Cleaning file structure.

--- obj/Debug/netstandard2.0/MyPluginInfo.cs
+++
@@ -1,9 +0,0 @@
-namespace LowCountProducts
-{
-    public static class MyPluginInfo
-    {
-        public const string PLUGIN_GUID = "LowCountProducts";
-        public const string PLUGIN_NAME = "My first plugin";
-        public const string PLUGIN_VERSION = "1.0.0";
-    }
-}
--- obj/Debug/netstandard2.1/MyPluginInfo.cs
+++
@@ -1,9 +0,0 @@
-namespace LowCountProducts
-{
-    public static class MyPluginInfo
-    {
-        public const string PLUGIN_GUID = "LowCountProducts";
-        public const string PLUGIN_NAME = "LowCountProducts";
-        public const string PLUGIN_VERSION = "0.0.1";
-    }
-}
